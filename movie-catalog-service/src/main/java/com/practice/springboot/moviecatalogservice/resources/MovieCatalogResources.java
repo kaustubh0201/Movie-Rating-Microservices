dc16eb39884be0d052c346ae72a58fbfcbfc35c5
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-21 02:09:26 +0530

    chore: models and resources done in the services;

---
+++ movie-catalog-service/src/main/java/com/practice/springboot/moviecatalogservice/resources/MovieCatalogResources.java
@@ -0,0 +1,22 @@
+package com.practice.springboot.moviecatalogservice.resources;
+
+import com.practice.springboot.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResources {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4)
+        );
+    }
+}
---
+++ ratings-data-service/src/main/java/com/practice/springboot/ratingsdataservice/resources/RatingsResources.java
@@ -0,0 +1,16 @@
+package com.practice.springboot.ratingsdataservice.resources;
+
+import com.practice.springboot.ratingsdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResources {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
