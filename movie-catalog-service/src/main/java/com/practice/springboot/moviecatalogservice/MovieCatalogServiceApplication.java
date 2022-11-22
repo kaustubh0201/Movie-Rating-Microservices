ce98af4a1412b5df8230a07a26c02000b77647a8
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-22 22:06:26 +0530

    chore: eureka client added to movie catalog service;

--- movie-catalog-service/src/main/java/com/practice/springboot/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/practice/springboot/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,13 @@ package com.practice.springboot.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	// beans are singleton in itself so if we need only one instance then we can use beans
