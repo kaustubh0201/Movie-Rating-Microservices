d9a65489b6aec54dc8313801748f641e48141ff2
kaustubh0201 <kaustubh1818@gmail.com>
2022-11-24 01:40:37 +0530

    chore: made separate services for Movie Info and User Rating;

--- movie-catalog-service/src/main/java/com/practice/springboot/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/com/practice/springboot/moviecatalogservice/resources/MovieCatalogResources.java
@@ -5,6 +5,8 @@ import com.practice.springboot.moviecatalogservice.models.CatalogItem;
 import com.practice.springboot.moviecatalogservice.models.Movie;
 import com.practice.springboot.moviecatalogservice.models.Rating;
 import com.practice.springboot.moviecatalogservice.models.UserRating;
+import com.practice.springboot.moviecatalogservice.services.MovieInfo;
+import com.practice.springboot.moviecatalogservice.services.UserRatingInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
@@ -30,55 +32,21 @@ public class MovieCatalogResources {
     private DiscoveryClient discoveryClient;
 
     @Autowired
-    private WebClient.Builder webClientBuilder;
+    private MovieInfo movieInfo;
+
+    @Autowired
+    private UserRatingInfo userRatingInfo;
 
     @RequestMapping("/{userId}")
     @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating ratings = getUserRating(userId);
+        UserRating ratings = userRatingInfo.getUserRating(userId);
 
         return ratings.getRatings()
                 .stream()
-                .map(this::getCatalogItem)
+                .map(rating -> movieInfo.getCatalogItem(rating))
                 .collect(Collectors.toList());
-    }
-
-    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
-    private UserRating getUserRating(String userId) {
-        return restTemplate.getForObject(
-                "http://ratings-data-service/ratingsdata/users/" + userId,
-                UserRating.class
-        );
-    }
-
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
-    private CatalogItem getCatalogItem(Rating rating) {
-        // restTemplate is for synchronous programming
-        // for each movie ID, call movie info service and get details
-        Movie movie = restTemplate.getForObject(
-                "http://movie-info-service/movies/" + rating.getMovieId(),
-                Movie.class
-        );
-
-        // web client is for asynchronous programming
-
-            /*
-
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8081/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
-            */
-
-        // put them all together
-        return new CatalogItem(
-                movie.getName(),
-                movie.getDescription(),
-                rating.getRating()
-        );
     }
 
     public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
@@ -88,19 +56,4 @@ public class MovieCatalogResources {
                 0
         ));
     }
-
-    private UserRating getFallbackUserRating(String userId) {
-        UserRating userRating = new UserRating();
-        userRating.setUserId(userId);
-        userRating.setRatings(Arrays.asList(
-                new Rating("0", 0)
-        ));
-
-        return userRating;
-    }
-
-    private CatalogItem getFallbackCatalogItem(Rating rating) {
-        return new CatalogItem("Movie name not found", "", rating.getRating());
-    }
-
 }
---
+++ movie-catalog-service/src/main/java/com/practice/springboot/moviecatalogservice/services/UserRatingInfo.java
@@ -0,0 +1,35 @@
+package com.practice.springboot.moviecatalogservice.services;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.practice.springboot.moviecatalogservice.models.Rating;
+import com.practice.springboot.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+public class UserRatingInfo {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    public UserRating getUserRating(String userId) {
+        return restTemplate.getForObject(
+                "http://ratings-data-service/ratingsdata/users/" + userId,
+                UserRating.class
+        );
+    }
+
+    public UserRating getFallbackUserRating(String userId) {
+        UserRating userRating = new UserRating();
+        userRating.setUserId(userId);
+        userRating.setRatings(Arrays.asList(
+                new Rating("0", 0)
+        ));
+
+        return userRating;
+    }
+}
